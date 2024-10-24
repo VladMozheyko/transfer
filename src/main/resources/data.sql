882d8e2d46b342e2617734716514da7654104ef3
Olgapnk <olechlapnk@yandex.ru>
2024-10-24 19:09:16 +0300

    test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO account VALUES (NULL, "Vlad", 11000);
-INSERT INTO account VALUES (NULL, "Katya", 1000 );
+INSERT INTO account VALUES (1, 'Vlad', 11000);
+INSERT INTO account VALUES (2, 'Katya', 1000 );
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,6 @@ create table account (
     name varchar(50) NOT NULL,
     amount DOUBLE NOT NULL
 );
+
+--INSERT INTO account VALUES (1, "Vlad", 11000);
+--INSERT INTO account VALUES (2, "Katya", 1000 );
